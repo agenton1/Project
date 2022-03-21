00db19c07e5d49e881871be127219f7f5111b21a
agenton1 <102045514+agenton1@users.noreply.github.com>
2022-03-21 20:51:17 +0100

    projetcine

---
+++ projectCine/src/projectcine/Controleur.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectcine;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author arthur
+ */
+public class Controleur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        
+         Connexion C = new Connexion("test","root","root");
+        // TODO code application logic here
+    }
+    
+}
---
+++ projectCine/src/projectcine/Fenetre.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectcine;
+
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author arthur
+ */
+public class Fenetre {
+    
+}
